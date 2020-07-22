6b0fd7e5c3bd39134d961e6b4d65af7abce474aa
Ben <bngarren@gmail.com>
2020-07-22 03:48:22 +0000

    Updates

---
+++ precache-manifest.2296c2948a8f544111a5a8deda3d1691.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a351aabfd9a9dc1a2c54dae0824f29",
+    "url": "/React-TodoApp/index.html"
+  },
+  {
+    "revision": "f17c4a745c8ac116b71c",
+    "url": "/React-TodoApp/static/css/main.5e3d6810.chunk.css"
+  },
+  {
+    "revision": "2f8e06061b9ffc9b3b50",
+    "url": "/React-TodoApp/static/js/2.a7c82f09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-TodoApp/static/js/2.a7c82f09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f17c4a745c8ac116b71c",
+    "url": "/React-TodoApp/static/js/main.1c090e5b.chunk.js"
+  },
+  {
+    "revision": "92fc80af526e36d5e5e8",
+    "url": "/React-TodoApp/static/js/runtime-main.4079f3fe.js"
+  }
+]);
